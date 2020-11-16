c5ee05a7cd309ec459aad6d70b08791997e902a5
nina-mir <ninamirf@gmail.com>
2020-11-16 15:31:43 -0800

    database initialized

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,6 +5,10 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
